558609bfbf4ea2565aa60e6dc77241c6b533c41d
Geordie Henderson <geordie.henderson@hootsuite.com>
2016-10-02 23:50:33 -0700

    Add JSON formatter

--- sloc.go
+++ sloc.go
@@ -56,6 +56,7 @@ var languages = []Language{
 	Language{"XML", mExt(".xml"), xmlComments},
 	Language{"CSS", mExt(".css"), cssComments},
 	Language{"JavaScript", mExt(".js"), cComments},
+	Language{"JSON", mExt(".json"), noComments},
 }
 
 type Commenter struct {
